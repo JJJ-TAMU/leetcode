fd38b1f30a611e17c5824f59577c2fc7ed50f4e4
JJJYXT <jiangjinjinyxt@gmail.com>
2021-09-07 15:32:22 -0500

    TopologicalSort

--- TopologicalSort/0310.java
+++ TopologicalSort/0310.java
@@ -1,6 +1,33 @@
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
         List<Integer> ans = new ArrayList<>();
+        // less than 3 nodes, at most one edge, both can be root
+        if (n < 3) {
+            for (int i = 0; i < n; i++) {   ans.add(i);     }
+            return ans;
+        }
+
+        int[] degrees = new int[n];
+        var graph = buildGraph(n, edges, degrees);
+
+        for (int i = 0; i < n; i++) {
+            // if is a leaf
+            if (degrees[i] == 1) {  ans.add(i); }
+        }
+
+        while (n > 2) {
+            n -= ans.size();
+            List<Integer> curr = new ArrayList<>();
+            for (int leaf : ans) {
+                for (int parent : graph[leaf]) {
+                    if (--degrees[parent] == 1) {
+                        curr.add(parent);
+                    }
+                }
+            }
+            ans = curr;
+        }
+        return ans;
     }
 
     private static List<Integer>[] buildGraph(int n, int[][] edges, int[] degrees) {
